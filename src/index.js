71e86cf38d51ee124180cafc5ff47ec7208e75ac
“thescriv” <thescriv@student.42.fr>
2020-08-29 22:53:49 +0200

    a

--- src/index.js
+++ src/index.js
@@ -6,16 +6,22 @@ import superagent from 'superagent'
 
 import './styles.css'
 
+const API_URL = process.env.API_URL || 'http://localhost:5000'
+
 function App() {
   const [camera, setCamera] = useState(false)
   const [result, setResult] = useState(null)
+  const [bookValue, setBookValue] = useState('')
 
   useEffect(() => {
     const _getBookValue = async () => {
-      const bookValue = await superagent.get(
-        `${process.env.API_URL}/getBarcode/${result}`
-      )
-      console.log(bookValue)
+      console.log(API_URL)
+      const {
+        body: { book_value },
+      } = await superagent.get(`${API_URL}/getBarcode/${result}`)
+
+      setBookValue(book_value)
+      console.log(book_value)
     }
 
     if (result) {
@@ -30,9 +36,12 @@ function App() {
   return (
     <div className='App'>
       <p>{result ? result : 'Scanning...'}</p>
+      <hr width='1' />
+      <p>{bookValue}</p>
       <button onClick={() => setCamera(!camera)}>
         {camera ? 'Stop' : 'Start'}
       </button>
+      <button onClick={() => setResult(null)}>reset</button>
       <div className='container'>
         {camera && <Scanner onDetected={onDetected} />}
       </div>
